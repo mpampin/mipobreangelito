cb024e80b3264a764324af3f51acedbbee86d956
Mauro Pampin <mauro.pampin@avantrip.com>
2017-06-12 15:38:42 -0300

    Se borran dos fotos y se fixea formulario de contacto

--- assets/form/js/contact-form.js
+++ assets/form/js/contact-form.js
@@ -9,8 +9,6 @@ jQuery(document).ready(function ($) {
         var showErrors = $f.attr('data-show-errors') == 'true';
         var hideForm = $f.attr('data-hide-form') == 'true';
 
-        var emailSubject = $f.attr('data-email-subject');
-
         var $submit = $f.find('[type="submit"]');
 
         //prevent double click
@@ -50,51 +48,13 @@ jQuery(document).ready(function ($) {
 
         $submit.addClass('disabled');
 
-        $f.append('<input class="temp" type="hidden" name="email_subject" value="' + emailSubject + '">');
-        $f.append('<input type="hidden" name="js" value="1">');
         $.ajax({
             url: $f.attr('action'),
-            method: 'post',
+            method: 'POST',
             data: $f.serialize(),
             dataType: 'json',
-            success: function (data) {
-                $('span.error', $f).remove();
-                $('.error', $f).removeClass('error');
-                $('.form-group', $f).removeClass('has-error');
+            /*success: function (data) {
 
-                if (data.errors) {
-                    $.each(data.errors, function (i, k) {
-                        if (i == 'global') {
-                            alert(k);
-                        } else {
-                            var input = $('[name^="' + i + '"]', $f).addClass('error');
-                            if (showErrors) {
-                                input.after('<span class="error help-block">' + k + '</span>');
-                            }
-
-                            if (input.parent('.form-group')) {
-                                input.parent('.form-group').addClass('has-error');
-                            }
-                        }
-                    });
-                } else {
-                    var item = data.success ? '.successMessage' : '.errorMessage';
-                    if (hideForm) {
-                        $f.fadeOut(function () {
-                            $f.parent().find(item).show();
-                        });
-                    } else {
-                        $f.parent().find(item).fadeIn();
-                        $f[0].reset();
-                    }
-                }
-
-                $submit.removeClass('disabled');
-                cleanupForm($f);
-
-                if (typeof nst_slider === "function") {
-                  $('.ct-range-slider').nstSlider('refresh');
-                }
             },
             error: function (data) {
                 if (debug) {
@@ -102,6 +62,44 @@ jQuery(document).ready(function ($) {
                 }
                 $submit.removeClass('disabled');
                 cleanupForm($f);
+            }*/
+        }).always(function(data) {
+            $('span.error', $f).remove();
+            $('.error', $f).removeClass('error');
+            $('.form-group', $f).removeClass('has-error');
+
+            if (data.errors) {
+                $.each(data.errors, function (i, k) {
+                    if (i == 'global') {
+                        alert(k);
+                    } else {
+                        var input = $('[name^="' + i + '"]', $f).addClass('error');
+                        if (showErrors) {
+                            input.after('<span class="error help-block">' + k + '</span>');
+                        }
+
+                        if (input.parent('.form-group')) {
+                            input.parent('.form-group').addClass('has-error');
+                        }
+                    }
+                });
+            } else {
+                var item = data.success ? '.successMessage' : '.errorMessage';
+                if (hideForm) {
+                    $f.fadeOut(function () {
+                        $f.parent().find(item).show();
+                    });
+                } else {
+                    $f.parent().find(item).fadeIn();
+                    $f[0].reset();
+                }
+            }
+
+            $submit.removeClass('disabled');
+            cleanupForm($f);
+
+            if (typeof nst_slider === "function") {
+                $('.ct-range-slider').nstSlider('refresh');
             }
         });
 
